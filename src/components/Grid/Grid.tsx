77417a7d0a25944e933ec7388e14700fe1c84ed5
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-04-10 13:23:08 +0300

    final additions and edits

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -4,8 +4,16 @@ import { createUseStyles } from 'react-jss';
 import styles from './styles';
 
 
-// should this be in this file?
+// should these two consts be in this file?
 const useStyles = createUseStyles(styles);
+
+const cardData = [
+    { LocationCardProp: "Tel Aviv", TempCardProp: 25, UVCardProp: 8 },
+    { LocationCardProp: "Johannesburg", TempCardProp: 6, UVCardProp: 4 },
+    { LocationCardProp: "Antarctica", TempCardProp: -5, UVCardProp: 0 },
+    { LocationCardProp: "Texas", TempCardProp: 31, UVCardProp: 12 },
+  ];
+
 
 const Grid = () => {
     const classes = useStyles();
@@ -14,13 +22,16 @@ const Grid = () => {
             <div className={classes.gridHeader}>
                 <h1>Weather</h1>
             </div>
-        {/* I know there is a better logical way to do this and to use API as apose static mock data - will come back to this if time */}
-          <div className={classes.grid}>
-            <Card LocationCardProp="Tel Aviv" TempCardProp={25} UVCardProp={8}/>
-            <Card LocationCardProp="Johannesburg" TempCardProp={6} UVCardProp={4}/>
-            <Card LocationCardProp="Antarctica" TempCardProp={-5} UVCardProp={0}/>
-            <Card LocationCardProp="Texas" TempCardProp={31} UVCardProp={12}/>
-          </div>
+            <div className={classes.grid}>
+                {cardData.map((card, index) => (
+                <Card
+                    key={index} 
+                    LocationCardProp={card.LocationCardProp}
+                    TempCardProp={card.TempCardProp}
+                    UVCardProp={card.UVCardProp}
+                />
+                ))}
+            </div>
         </>
     )
 }
