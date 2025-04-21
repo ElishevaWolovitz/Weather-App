25e1ab9ea2ce88cc2dea33a7a14f59c2856139c3
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-04-21 13:37:07 +0300

    added consts for Temperature details at each range

---
+++ src/const.ts
@@ -0,0 +1,49 @@
+export const TemperatureDetailsFreezing = {
+    "min": -Infinity, 
+    "max": 0, 
+    "backgroundImg": {
+        "src": "./assests/backgroundImages/FreezingBGImg.jpg", 
+        "alt": "Freezing background image"
+    },
+    "icon": {
+        "src": "./assests/weatherIcons/FreezingIcon.png", 
+        "alt": "snowflake icon"
+    } 
+}
+export const TemperatureDetailsCold = {
+    "min": 0, 
+    "max": 15, 
+    "backgroundImg": {
+        "src": "./assests/backgroundImages/ColdBGImg.jpg", 
+        "alt": "Cold background image"
+    },
+    "icon": {
+        "src": "./assests/weatherIcons/ColdIcon.png", 
+        "alt": "rain icon"
+    } 
+}
+
+export const TemperatureDetailsNormal = {
+    "min": 15, 
+    "max": 30, 
+    "backgroundImg": {
+        "src": "./assests/backgroundImages/NormalBGImg.jpg", 
+        "alt": "Normal temp background image"
+    },
+    "icon": {
+        "src": "./assests/weatherIcons/NormalIcon.png", 
+        "alt": "sun icon"
+    } 
+}
+export const TemperatureDetailsHot = {
+    "min": 30,
+    "max": Infinity,
+    "backgroundImg": {
+        "src": "./assests/backgroundImages/HotBGImg.jpg",
+        "alt": "Hot background image"
+    },
+    "icon": {
+        "src": "./assests/weatherIcons/HotIcon.png",
+        "alt": "thermometer icon"
+    }
+}
