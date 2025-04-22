264ae61e0f2a8f71e50462f9ca80d2d4a2b63adf
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-04-22 14:56:24 +0300

    took out CarProps from Grid types

--- src/components/Grid/types.ts
+++ src/components/Grid/types.ts
@@ -1,11 +1,4 @@
 
-export type CardProps = {
-    location: string;
-    temperature: number;
-    uv: number; 
-    backgroundImg: ImageObjProps; 
-    icon: ImageObjProps;
-  };
 
   export type CardDataProps = {
     location: string;
