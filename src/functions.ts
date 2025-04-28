6ba09747f9ed882490473d25ae641dd366e14f3b
ElishevaWolovitz <elishevawolovitz@gmail.com>
2025-04-28 14:22:23 +0300

    simplified global function

--- src/functions.ts
+++ src/functions.ts
@@ -1,15 +1,10 @@
 
  
-export const findCompare = <T extends {min: number; max: number}>(compareVal: number, array: T[]): T | undefined => {
-    for (const element of array)
-    {
-        if (element.min <= compareVal && element.max >= compareVal)
-            return element;
-    } 
-}
 export const getObjInRange = <T extends {min: number; max:number}>(compareVal: number, objArray: T[], defaultObj: T): T => {
-    const objWithRange = findCompare(compareVal, objArray);
-    if(objWithRange)
-        return objWithRange;
+    for (const element of objArray)
+        {
+            if (element.min <= compareVal && element.max >= compareVal)
+                    return element; 
+        } 
     return defaultObj;
 }
